70a35813c25a7bb5147c61d79952d6746c271c18
dvmoomoodv <dvmoomoodv@gmail.com>
2020-08-07 15:08:31 +0900

    Updates

---
+++ precache-manifest.dd2d72aab89bc73e9ff58513db465b08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160d7e90d5faecb1c5b0836318795f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5990b9ca0bec1b58d7ae",
+    "url": "/static/css/main.d84cb2e8.chunk.css"
+  },
+  {
+    "revision": "13477a071fb6f65f08d4",
+    "url": "/static/js/2.ffba5a0b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ffba5a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5990b9ca0bec1b58d7ae",
+    "url": "/static/js/main.be30fd07.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "f35a158e545cb9f3d52a367692e597aa",
+    "url": "/static/media/bg.f35a158e.jpg"
+  }
+]);
